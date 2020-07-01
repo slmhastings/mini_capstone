e1e7307d1d2688c8e66e50bf0d534b48b9e0de03
Sequona Hastings <slmhastings@gmail.com>
2020-06-30 21:27:28 -0500

    add all products

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,10 @@
 class Api::ProductsController < ApplicationController
+  
+  def all
+    @products = Product.all
+    render 'product_info.json.jb'
+  end
+
+  def first_product
+    @product = Product.first
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/all_products" => "products#all"
+  end
+
+  namespace :api do
+    get ""
+  end
 end
