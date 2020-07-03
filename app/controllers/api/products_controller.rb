25b4a0db3fabbfe4e8f27782f0c660ce957d9266
Sequona Hastings <slmhastings@gmail.com>
2020-07-02 21:08:03 -0500

    add REST convention

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,10 +1,17 @@
 class Api::ProductsController < ApplicationController
   
-  def all
+  def index
     @products = Product.all
-    render 'product_info.json.jb'
+    render 'index.json.jb'
   end
 
-  def first_product
-    @product = Product.first
+  # def oneproduct
+  #   @product = Product.first
+  #   render 'oneproduct.json.jb'
+  # end
+
+  # def show_product
+  #   @item = params['item']
+  #   render 'product_shown.json.jb'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/all_products" => "products#all"
+    get "/products" => "products#index"
+    
   end
 
-  namespace :api do
-    get ""
-  end
 end
