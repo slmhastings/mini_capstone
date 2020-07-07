aacbd28290fd5ebf0aa322609765445f72564cec
Sequona Hastings <slmhastings@gmail.com>
2020-07-06 20:52:10 -0500

    add model

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -24,10 +24,10 @@ class Api::ProductsController < ApplicationController
 
   def update
     @product = Product.find_by(id: params[:id])
-    @product.name = params[:name]
-    @product.price = params[:price]
-    @product.image_url = params[:image_url]
-    @product.description = params[:description]
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price] || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
     @product.save
     render 'show.json.jb'
   end
